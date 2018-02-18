2b3495de29da150a5bc11bacaf31e7b0eee43e56
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-18 18:25:53 -0500

    Fixed mistake in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = express()
     .use((req, res) => res.sendFile(INDEX) )
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO.server();
+const io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('Client connected');
