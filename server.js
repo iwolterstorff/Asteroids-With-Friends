0e9378c096249e9ab9897f0321f3a8fcc14bf8c7
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-18 18:36:26 -0500

    Socket.io works!

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const SocketServer = require('ws').Server;
 const socketIO = require('socket.io');
 const path = require('path');
 
@@ -10,26 +9,13 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
     .use((req, res) => res.sendFile(INDEX) )
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// const io = socketIO(server);
-//
-// io.on('connection', (socket) => {
-//     console.log('Client connected');
-//     socket.on('disconnect', () => console.log('Client disconnected'));
-// });
-//
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+const io = socketIO(server);
 
-const wss = new SocketServer({ server });
-
-wss.on('connection', (ws) => {
+io.on('connection', (socket) => {
     console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-});
-}, 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
