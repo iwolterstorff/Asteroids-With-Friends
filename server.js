4b7501bfbeaf9d89c9dfd146c520d711108a5beb
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-18 18:31:05 -0500

    Trying with regular websockets library

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const SocketServer = require('ws').Server;
 const socketIO = require('socket.io');
 const path = require('path');
 
@@ -11,11 +12,24 @@ const server = express()
     .use((req, res) => res.sendFile(INDEX) )
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+// const io = socketIO(server);
+//
+// io.on('connection', (socket) => {
+//     console.log('Client connected');
+//     socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+//
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-io.on('connection', (socket) => {
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
     console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+    ws.on('close', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => {
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+});
+}, 1000);
