9884b8ac0e18ef3782709e81c8a73ef2cfb11a10
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-18 19:50:38 -0500

    Getting chat tutorial to work again

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const PORT = process.env.port || 3000;
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -12,6 +14,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+http.listen(PORT, function(){
+    console.log(`listening on port ${ PORT }`);
+})
