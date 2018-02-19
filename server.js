f9881da456fbe8eea3fe2ceb94241fa27a454a46
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-18 19:40:41 -0500

    chat tut

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-'use strict';
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-    .use((req, res) => res.sendFile(INDEX) )
-.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-socket.on('disconnect', () => console.log('Client disconnected'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
