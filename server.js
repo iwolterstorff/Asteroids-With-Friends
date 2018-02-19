aff2e338a6c7ed2da09d45aacfa4407c3d1a07e6
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-18 20:35:23 -0500

    Playing with graphics now

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var express = require('express')();
+var server = require('http').Server(express);
 
-app.get('/', function(req, res){
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT);
+
+express.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
-});
-
-http.listen(port, function(){
-    console.log('listening on *:' + port);
 });
