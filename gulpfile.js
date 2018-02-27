0baaad404b75ab24fa2ffd495771ac529f7af849
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-26 20:11:46 -0500

    Moved things around

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var todo = require('gulp-todo');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', () => {
     // Default task code goes here
+});
+
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js', '!node_modules/**/*.js', '!bin/**/*.js'])
+        .pipe(jshint({
+            esversion: 6
+        }))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('todo', () => {
+    gulp.src('**/*.js')
+        .pipe(todo())
+        .pipe(gulp.dest('./'));
 });
