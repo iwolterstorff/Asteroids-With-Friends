a3a291c89af7c02b93784d4ee1698f8b42229d4c
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-02-27 21:21:59 -0500

    Added a lot of gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,35 @@
-var gulp = require('gulp');
-var todo = require('gulp-todo');
-var jshint = require('gulp-jshint');
+const gulp = require('gulp');
+
+const plugins = require('gulp-load-plugins')();
 
 gulp.task('default', () => {
     // Default task code goes here
 });
 
 gulp.task('lint', () => {
-    return gulp.src(['**/*.js', '!node_modules/**/*.js', '!bin/**/*.js'])
-        .pipe(jshint({
+    return gulp.src(['**/*.js', '!node_modules/**/*.js', '!bin/**/*.js', '!src/client/lib/**/*.js'])
+        .pipe(plugins.jshint({
             esversion: 6
         }))
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
+        .pipe(plugins.jshint.reporter('jshint-stylish'));
+
+        // Uncomment this and remove semicolon above to have JSLint fail when warnings are unresolved
+        // .pipe(plugins.jshint.reporter('fail'));
+});
+
+gulp.task('run', ['lint'], () => {
+    plugins.nodemon({
+        delay: 10,
+        script: './server/server.js',
+        ext: 'html js css'
+    })
+        .on('restart', () => {
+            plugins.util.log('server restarted');
+        });
 });
 
 gulp.task('todo', () => {
     gulp.src('**/*.js')
-        .pipe(todo())
+        .pipe(plugins.todo())
         .pipe(gulp.dest('./'));
 });
